a1ae9c2589dad81b6e9e5da815e7232c5577eb7c
Callum Anderson <callum_anderson@hotmail.com>
2018-08-24 19:42:43 +0100

    Added update(), show_info() and show_info_by_id() methods to GymMember class

--- console.rb
+++ console.rb
@@ -1,13 +1,6 @@
 require_relative('db/sql_runner.rb')
 require_relative('model/GymMember.rb')
 
-def test
-  sql_string = "SELECT * FROM gym_classes"
-  puts "test"
-  puts SqlRun.sql_run(sql_string)
-end
-
-p GymMember.view_all.values
 
 # johndoe = GymMember.new({'first_name' => 'John', 'last_name' => 'Doe',
 #                 'age' => 28, 'contact_number' => '07742387421', 'email' => 'johndoe@gmail.com'})
--- model/GymMember.rb
+++ model/GymMember.rb
@@ -37,4 +37,29 @@ attr_accessor('first_name', 'last_name', 'age', 'email', 'contact_number')
     sql_return = SqlRun.sql_run(sql_string)
     return sql_return
   end
+
+  def show_info()
+    sql_string = "SELECT * FROM members
+                  WHERE id = $1"
+    values = [@id]
+    sql_return = SqlRun.sql_run(sql_string, values)
+    return sql_return[0]
+  end
+
+  def self.show_info_by_id(id)
+    sql_string = "SELECT * FROM members
+                  WHERE id = $1"
+    values = [id]
+    sql_return = SqlRun.sql_run(sql_string, values)
+    return sql_return[0]
+  end
+
+  def update()
+    sql_string = "UPDATE members
+                  SET (first_name, last_name, age, contact_number, email)
+                  = ($1,$2,$3,$4,$5) WHERE id = $6"
+    values = [@first_name, @last_name, @age, @email, @contact_number, @id]
+    SqlRun.sql_run(sql_string, values)
+  end
+
 end
