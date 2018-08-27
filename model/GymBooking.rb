7a7b54eec44a8b35377bc6d6f88f777d042ee017
Callum Anderson <callum_anderson@hotmail.com>
2018-08-27 13:13:08 +0100

    Renamed foreign keys in gym_bookings table and refactored code to incorporate

--- model/GymBooking.rb
+++ model/GymBooking.rb
@@ -7,7 +7,7 @@ class GymBooking
   end
 
   def add_to_db()
-    sql_string = "INSERT INTO gym_bookings (gym_class, member)
+    sql_string = "INSERT INTO gym_bookings (gym_class_id, member_id)
                   VALUES ($1, $2) RETURNING id"
     values = [@gym_class, @member]
     sql_return = SqlRun.sql_run(sql_string, values)
@@ -35,7 +35,7 @@ class GymBooking
 
   def self.return_booking_id(gym_class_id, member_id)
     sql_string = "SELECT id FROM gym_bookings
-                  WHERE gym_class = $1 AND member = $2"
+                  WHERE gym_class_id = $1 AND member_id = $2"
     values = [gym_class_id, member_id]
     return SqlRun.sql_run(sql_string, values)[0]['id'].to_i()
   end
@@ -43,9 +43,9 @@ class GymBooking
   def self.show_all_bookings()
     sql_string = "SELECT m.first_name, m.last_name, c.name, b.id
                   FROM members AS m INNER JOIN gym_bookings AS b
-                  ON m.id = b.member
+                  ON m.id = b.member_id
                   INNER JOIN gym_classes AS c
-                  ON b.gym_class = c.id"
+                  ON b.gym_class_id = c.id"
     sql_return = SqlRun.sql_run(sql_string)
     return sql_return
   end
